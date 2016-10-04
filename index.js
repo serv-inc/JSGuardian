ccc676eb340717f73ff84f76ef7f6a706eb3bf9e
serv.inc <serv-inc@users.noreply.github.com>
2016-10-04 11:40:05 +0200

    lacked whitelist

--- index.js
+++ index.js
@@ -3,27 +3,29 @@ const Simple = require('sdk/simple-prefs');
 
 Simple.on("limit", onPrefChange);
 Simple.on("multi", onPrefChange);
+Simple.on("whitelist", onPrefChange);
 
 let mod = create();
 
 function create(limit) {
     return require("sdk/page-mod").PageMod({
-    include: /.*/,
-    attachTo: "top",
-    contentScriptFile: "./pf.js",
-    contentScriptOptions: {
-        limit: Simple.prefs.limit,
-        regexes: [m(Simple.prefs.point2), m(Simple.prefs.point3),
-                  m(Simple.prefs.point5), m(Simple.prefs.point10),
-                  m(Simple.prefs.point20), m(Simple.prefs.point25),
-                  m(Simple.prefs.point30), m(Simple.prefs.point40),
-                  m(Simple.prefs.point50), m(Simple.prefs.point60),
-                  m(Simple.prefs.point70), m(Simple.prefs.point80),
-                  m(Simple.prefs.point90), m(Simple.prefs.point100),
-                  m(Simple.prefs.point120), m(Simple.prefs.point130),
-                  m(Simple.prefs.point150)],
-        regexValues: [2,3,5,10,20,25,30,40,50,60,70,80,90,100,120,130,150]
-    }});
+        include: /.*/,
+        exclude: RegExp(Simple.prefs.whitelist),
+        attachTo: "top",
+        contentScriptFile: "./pf.js",
+        contentScriptOptions: {
+            limit: Simple.prefs.limit,
+            regexes: [ml(Simple.prefs.point2), ml(Simple.prefs.point3),
+                      ml(Simple.prefs.point5), ml(Simple.prefs.point10),
+                      ml(Simple.prefs.point20), ml(Simple.prefs.point25),
+                      ml(Simple.prefs.point30), ml(Simple.prefs.point40),
+                      ml(Simple.prefs.point50), ml(Simple.prefs.point60),
+                      ml(Simple.prefs.point70), ml(Simple.prefs.point80),
+                      ml(Simple.prefs.point90), ml(Simple.prefs.point100),
+                      ml(Simple.prefs.point120), ml(Simple.prefs.point130),
+                      ml(Simple.prefs.point150)],
+            regexValues: [2,3,5,10,20,25,30,40,50,60,70,80,90,100,120,130,150]
+        }});
 }
 
 function onPrefChange() {
@@ -32,7 +34,7 @@ function onPrefChange() {
 }
 
 /** makes regexes multiline */
-function m(reg) {
+function ml(reg) {
     if ( Simple.prefs.multi ) {
 	return reg.replace('.*', '[^]*');
     } else {
