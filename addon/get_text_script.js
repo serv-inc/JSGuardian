bad247e9a767cf04e595dfba05b5ede8ce5b039a
serv-inc <serv-inc@users.noreply.github.com>
2017-03-17 14:49:50 +0100

    last version lacked logo

--- addon/get_text_script.js
+++ addon/get_text_script.js
@@ -1,5 +1,7 @@
 "use strict";
 /* jshint esversion: 6, strict: global */
 /* globals chrome */
+/* globals document */
+// licensed under the MPL 2.0 by (github.com/serv-inc)
 
 chrome.runtime.sendMessage(document.body.innerText ||document.body.textContent);
