0781c223bc2eb20fdc0e21cb603b418fdbd9e555
serv-inc <serv-inc@users.noreply.github.com>
2017-04-29 16:39:48 +0200

    lacked possibility to change the scoring words

--- addon/options.js
+++ addon/options.js
@@ -4,28 +4,43 @@
 /* globals console */
 /* globals document */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
-
-const DEFAULTS = {
-    "whitelist": 'mozilla.org|dansguardian.org',
-    "limit": 160
-};
+let initError = false;
 
 function saveOptions(e) {
     e.preventDefault();
-    chrome.storage.local.set({
+    if ( initError ) {
+        console.error("did not save erroneous options");
+        return;
+    }
+    var settings = {
+        blockvals: [],
         limit: document.querySelector("#limit").value,
-        whitelist: document.querySelector("#whitelist").value
+        whitelist: document.querySelector("#whitelist").value.toString().slice(1, -1)
+    };
+    let old_settings = chrome.extension.getBackgroundPage().getSettings();
+    [2,3,5,10,20,25,30,40,50,60,70,80,90,100,120,130,150].forEach(function(id){
+        settings.blockvals.push({
+            name: id,
+            value: document.querySelector("#p" + id).value
+        });
     });
+    chrome.storage.local.set(settings);
 }
 
 function restoreOptions() {
-
-    function setCurrentChoice(result) {
-        document.querySelector("#limit").value = result.limit || DEFAULTS.limit;
-        document.querySelector("#whitelist").value = result.whitelist || DEFAULTS.whitelist;
+    if ( ! chrome ) {
+        console.error("error on option initialization");
+        initError = true;
+        return;
     }
-
-    chrome.storage.local.get(null, setCurrentChoice);
+    initError = false;
+    let settings = chrome.extension.getBackgroundPage().getSettings();
+    document.querySelector("#limit").value = settings.limit;
+    document.querySelector("#whitelist").value = settings.whitelist;
+    settings.blockvals.forEach(function(el) {
+        document.querySelector("#p" + el.name).value = el.value;
+    });
+    
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
