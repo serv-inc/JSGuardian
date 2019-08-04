4fe29050a7a21bcc01c28dc3a3d14d11ea37cc4e
serv-inc <serv-inc@users.noreply.github.com>
2019-08-04 23:20:14 +0200

    refactor: jshint was less clean

--- addon/get_text_script.js
+++ addon/get_text_script.js
@@ -1,8 +1,6 @@
 "use strict";
 /* jshint esversion: 6, strict: global */
-/* globals chrome */
-/* globals document */
-/* globals window */
+/* globals chrome, document, window */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
 
 function send() {
