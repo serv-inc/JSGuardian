1e63a5ee2cd5014f6b3e158a1187e23b510444c3
serv-inc <serv-inc@users.noreply.github.com>
2020-01-04 11:01:47 +0100

    test prettier

--- addon/scorer.test.js
+++ addon/scorer.test.js
@@ -1,25 +1,21 @@
-const {Worker} = require("worker_threads")
+const { Worker } = require("worker_threads");
 
-test('scan matches', done => {
-  const scorer = new Worker("./addon/scorer.js", {"workerData": "hello"})
+test("scan matches", done => {
+  const scorer = new Worker("./addon/scorer.js", { workerData: "hello" });
   scorer.on("message", val => {
-    expect(val).toBe(1)
-    scorer.terminate().then(_ =>
-      done()
-    )
-  })
-  scorer.postMessage(JSON.stringify(["init", "hello"]))
-  scorer.postMessage(JSON.stringify(["scan", "hello"]))
-})
+    expect(val).toBe(1);
+    scorer.terminate().then(_ => done());
+  });
+  scorer.postMessage(JSON.stringify(["init", "hello"]));
+  scorer.postMessage(JSON.stringify(["scan", "hello"]));
+});
 
-test('scan does not match', done => {
-  const scorer = new Worker("./addon/scorer.js", {"workerData": "hello"})
+test("scan does not match", done => {
+  const scorer = new Worker("./addon/scorer.js", { workerData: "hello" });
   scorer.on("message", val => {
-    expect(val).toBe(0)
-    scorer.terminate().then(_ =>
-      done()
-    )
-  })
-  scorer.postMessage(JSON.stringify(["init", "hello"]))
-  scorer.postMessage(JSON.stringify(["scan", "asdf"]))
-})
+    expect(val).toBe(0);
+    scorer.terminate().then(_ => done());
+  });
+  scorer.postMessage(JSON.stringify(["init", "hello"]));
+  scorer.postMessage(JSON.stringify(["scan", "asdf"]));
+});
