e986a72fcd06725ff75819d0d9a33e4b4d41b6b6
serv-inc <serv-inc@users.noreply.github.com>
2020-01-04 06:14:45 +0100

    first actor with test

---
+++ addon/scorer.js
@@ -0,0 +1,17 @@
+const {parentPort, workerData} = require("worker_threads")
+// console.log("data", workerData)
+let regex
+//console.log(regex)
+
+parentPort.on('message', (json) => {
+  var [task, options] = JSON.parse(json)
+  if (task === "init") {
+    regex = RegExp(workerData, "gi");
+  } else if (task === "scan") {
+//    console.log("r2", regex)
+    parentPort.postMessage(options.match(regex).length)
+  } else {
+    throw new Error("wut: " + task)
+  }
+})
+
