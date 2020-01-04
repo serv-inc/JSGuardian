794fbdbecb8c71cb2c698b29baa14d0c06d07572
serv-inc <serv-inc@users.noreply.github.com>
2020-01-04 08:54:02 +0100

    prettier

--- addon/scorer.js
+++ addon/scorer.js
@@ -1,17 +1,16 @@
-const {parentPort, workerData} = require("worker_threads")
+const { parentPort, workerData } = require("worker_threads");
 // console.log("data", workerData)
-let regex
+let regex;
 //console.log(regex)
 
-parentPort.on('message', (json) => {
-  var [task, options] = JSON.parse(json)
+parentPort.on("message", json => {
+  var [task, options] = JSON.parse(json);
   if (task === "init") {
     regex = RegExp(workerData, "gi");
   } else if (task === "scan") {
 //    console.log("r2", regex)
     parentPort.postMessage(options.match(regex).length)
   } else {
-    throw new Error("wut: " + task)
+    throw new Error("wut: " + task);
   }
-})
-
+});
