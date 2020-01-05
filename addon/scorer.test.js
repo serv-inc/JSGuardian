a47b31857599003aedc2a9e18c4190bd6a969bac
serv-inc <serv-inc@users.noreply.github.com>
2020-01-05 17:44:25 +0100

    one more test, working with node+mocha

--- addon/scorer.test.js
+++ addon/scorer.test.js
@@ -1,21 +1,46 @@
-const { Worker } = require("worker_threads");
+if (typeof window === "undefined") {
+  var Worker = require("tiny-worker");
+  var assert = require("assert");
+}
 
-test("scan matches", done => {
-  const scorer = new Worker("./addon/scorer.js", { workerData: "hello" });
-  scorer.on("message", val => {
-    expect(val).toBe(1);
-    scorer.terminate().then(_ => done());
-  });
-  scorer.postMessage(JSON.stringify(["init", "hello"]));
-  scorer.postMessage(JSON.stringify(["scan", "hello"]));
+const FILE = "./addon/scorer.js";
+
+it("scan initializes", done => {
+  const scorer = new Worker(FILE);
+  scorer.onmessage = function(val) {
+    if (val.data[0] === "init done") {
+      scorer.terminate();
+      done();
+    }
+  };
+  scorer.postMessage(["init", "hello"]);
+  scorer.postMessage(["scan", "hello"]);
 });
 
-test("scan does not match", done => {
-  const scorer = new Worker("./addon/scorer.js", { workerData: "hello" });
-  scorer.on("message", val => {
-    expect(val).toBe(0);
-    scorer.terminate().then(_ => done());
-  });
-  scorer.postMessage(JSON.stringify(["init", "hello"]));
-  scorer.postMessage(JSON.stringify(["scan", "asdf"]));
+it("scan does match once", done => {
+  const scorer = new Worker(FILE);
+  scorer.onmessage = function(val) {
+    if (val.data[0] === "scan done") {
+      assert.equal(val.data[1], 1); //    val.data.should.equal(0);
+      scorer.terminate();
+      done();
+    }
+  };
+
+  scorer.postMessage(["init", "hello"]);
+  scorer.postMessage(["scan", "hello"]);
+});
+
+it("scan does not match", done => {
+  const scorer = new Worker(FILE);
+  scorer.onmessage = function(val) {
+    if (val.data[0] === "scan done") {
+      assert.equal(val.data[1], 0); //    val.data.should.equal(0);
+      scorer.terminate();
+      done();
+    }
+  };
+
+  scorer.postMessage(["init", "hello"]);
+  scorer.postMessage(["scan", "asdf"]);
 });
