66cb2dc7249c4368758934c271fff6d3ddac6af4
serv-inc <serv-inc@users.noreply.github.com>
2020-01-05 17:43:39 +0100

    remove nodejs code from (prod) scorer.js

--- addon/scorer.js
+++ addon/scorer.js
@@ -1,39 +1,18 @@
-// nodejs code to use same api as web workers
-if (isNode()) {
-  var { parentPort } = require("worker_threads");
-}
-if (typeof onmessage === "undefined" && isNode()) {
-  var onmessage = function(action) {
-    parentPort.on("message", action);
-  };
-}
-if (typeof postMessage === "undefined" && isNode()) {
-  var postMessage = function(message) {
-    parentPort.postMessage(message);
-  };
-}
-
 let regex;
 
-onmessage(json => {
-  var [task, options] = JSON.parse(json);
+onmessage = function(e) {
+  var [task, options] = e.data;
   if (task === "init") {
     regex = RegExp(options, "gi");
+    postMessage(["init done"]);
   } else if (task === "scan") {
     const matches = options.match(regex);
     if (matches === null) {
-      parentPort.postMessage(0);
+      postMessage(["scan done", 0]);
     } else {
-      parentPort.postMessage(matches.length);
+      postMessage(["scan done", matches.length]);
     }
   } else {
     throw new Error("wut: " + task);
   }
-});
-
-function isNode() {
-  return (
-    typeof process !== "undefined" &&
-    process.release.name.search(/node|io.js/) !== -1
-  );
-}
+};
