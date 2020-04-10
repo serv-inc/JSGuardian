16194ae7112a138fc48451ad8ce017611b355ad4
serv-inc <serv-inc@users.noreply.github.com>
2020-04-10 23:34:17 +0200

    min interval in re-sending text

--- addon/get_text_script.js
+++ addon/get_text_script.js
@@ -3,10 +3,17 @@
 /* globals chrome */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
 
+var lastSent = 0;
+/** send at most this often (milliseconds) */
+const MIN_INTERVAL = 500;
+
 function send() {
-  chrome.runtime.sendMessage(
-    document.body.innerText || document.body.textContent
-  );
+  if ( Date.now() - MIN_INTERVAL > lastSent ) {
+    chrome.runtime.sendMessage(
+      document.body.innerText || document.body.textContent
+    );
+    lastSent = Date.now();
+  }
 }
 
 send();
