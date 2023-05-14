9ae223e833c88ddc59e9d44b4b7534e28f365573
serv-inc <serv-inc@users.noreply.github.com>
2023-05-14 10:59:26 +0200

    had more lint

--- addon/get_text_script.js
+++ addon/get_text_script.js
@@ -1,6 +1,6 @@
 "use strict";
 /* jshint esversion: 6, strict: global */
-/* globals chrome, document, window */
+/* globals chrome */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
 
 function send() {
