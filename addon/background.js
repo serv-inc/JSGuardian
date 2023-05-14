c7d82061cc291ee39e574c2237fb678a9f053396
serv-inc <serv-inc@users.noreply.github.com>
2023-05-14 10:59:26 +0200

    Revert "first attempt at promises"
    This reverts commit e7186511023bcc7442829dd46e2dd5758c1d30e3.

--- addon/background.js
+++ addon/background.js
@@ -9,7 +9,7 @@
  */
 const URL_RE = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 
-const BLOCKPAGE_URL = 'blockpage.html';
+const BLOCKPAGE_URL = "blockpage.html";
 const NO_SCAN = /extension:/;
 
 /** add URLS, and check whether added */
@@ -19,35 +19,37 @@ class BlockCache {
   }
 
   add(url) {
-    if ( this.allow(url) ) {
+    if (this.allow(url)) {
       this._cache.push(url);
     }
   }
 
   allow(url) {
-    return ! this._cache.includes(url);
+    return !this._cache.includes(url);
   }
 }
 let blockCache = new BlockCache();
 
 chrome.runtime.onMessage.addListener(function(pageText, sender) {
-  if ( ! getSettings().whitelistRegExp.test(sender.url)
-       && ! NO_SCAN.test(sender.url) ) {
-    if ( blockCache.allow(sender.url) ) {
+  if (
+    !getSettings().whitelistRegExp.test(sender.url) &&
+    !NO_SCAN.test(sender.url)
+  ) {
+    if (blockCache.allow(sender.url)) {
       scan(pageText, sender);
     } else {
-      setBlockPage(sender, ['cached site']);
+      setBlockPage(sender, ["cached site"]);
     }
   }
 });
 
 // how to validate blockpage? new URL(blockpage) is not available?
-function setBlockPage(sender, phraseArray=[''], limit="???") {
-  if ( NO_SCAN.test(sender.url) ) {
+function setBlockPage(sender, phraseArray = [""], limit = "???") {
+  if (NO_SCAN.test(sender.url)) {
     return;
   }
 
-  if ( blockCache.allow(sender.url) ) {
+  if (blockCache.allow(sender.url)) {
     blockCache.add(sender.url);
   }
 
@@ -55,43 +57,52 @@ function setBlockPage(sender, phraseArray=[''], limit="???") {
   if (isValid(getSettings().blockpage)) {
     blockpage = getSettings().blockpage;
   } else {
-    blockpage = chrome.extension.getURL(BLOCKPAGE_URL)
-      + '?' + encodeURIComponent(sender.tab.url)
-      + '&' + encodeURIComponent(JSON.stringify(phraseArray))
-      + '&' + limit;
+    blockpage =
+      chrome.extension.getURL(BLOCKPAGE_URL) +
+      "?" +
+      encodeURIComponent(sender.tab.url) +
+      "&" +
+      encodeURIComponent(JSON.stringify(phraseArray)) +
+      "&" +
+      limit;
   }
-  chrome.tabs.update(sender.tab.id, {'url': blockpage});
+  chrome.tabs.update(sender.tab.id, { url: blockpage });
 }
 
 function isValid(urlString) {
   return typeof urlString === "string" && URL_RE.test(urlString);
 }
 
-// td: this is non-testable due to i, score, ... above (and matches),
-// maybe refactor
-function scan(pageText, sender, score=0, matches=[]) {
-  var threads = [];
-  getSettings().blockvals.forEach(
-    (blockval) => {
-      var t =_do_score(pageText, blockval, matches);
-      threads.push(t);
-      t.then(singlescore => {
-        score += singlescore;
-        if ( score > getSettings().limit ) {
-          setBlockPage(sender, matches, ">= " + score);
-        }
-      });
-    }
-  );
+// td: this is non-testable due to i, score, ... above, maybe refactor
+function scan(
+  pageText,
+  sender,
+  score = 0,
+  matches = [],
+  i = getSettings().blockvals.length - 1
+) {
+  score += _do_score(pageText, getSettings().blockvals[i], matches);
+
+  if (score > getSettings().limit) {
+    setBlockPage(sender, matches, ">= " + score);
+  }
+
+  if (i > 0) {
+    setTimeout(function() {
+      scan(pageText, sender, score, matches, i - 1);
+    }, 0);
+  }
 }
 
 /** @return score of this blockObject on text */
-async function _do_score(pageText, blockObject, all_matches) {
+function _do_score(pageText, blockObject, all_matches) {
   let tmp = pageText.match(RegExp(blockObject.value, "gi"));
   let matches = new Set();
-  if ( tmp !== null ) {
-    tmp.forEach((el) => {matches.add(el.toLowerCase());});
+  if (tmp !== null) {
+    tmp.forEach(el => {
+      matches.add(el.toLowerCase());
+    });
   }
-  matches.forEach((el) => all_matches.push(el));
+  matches.forEach(el => all_matches.push(el));
   return matches.size * blockObject.name;
 }
